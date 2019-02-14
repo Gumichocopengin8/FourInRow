c00a7d53c6878d428ae99abd8f06aeea013953ee
nona <iKonnyaku40@yahoo.co.jp>
2019-02-14 04:32:11 -0500

    minor fixes: forgot to rename file name lol

---
+++ FourInRow/BasePiece.cs
@@ -0,0 +1,7 @@
+namespace FourInRow {
+  public abstract class BasePiece {
+    public abstract string Name { get; }
+
+    public abstract string PieceColor { get; }
+  }
+}
--- FourInRow/Piece.cs
+++
@@ -1,7 +0,0 @@
-namespace FourInRow {
-  public abstract class BasePiece {
-    public abstract string Name { get; }
-
-    public abstract string PieceColor { get; }
-  }
-}
